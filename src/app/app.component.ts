c1b47bdd72552ebe958d6315f6e9730a55a5d0ac
Eymux <eymux2009@gmail.com>
2017-12-12 14:06:06 +0100

    Integration von Dexie für Local Storage.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from './services/storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private db: StorageService) {
+  }
+
+  async ngOnInit(): Promise<void> {
+    this.db.reset().then(() => {
+      this.db.getPAL().then(pal => {
+        console.log(pal);
+      });
+    });
+  }
 }
