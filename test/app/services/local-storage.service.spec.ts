6fc9c39081e348e5c38d2a9295834b3d7f7f9ecf
Daniel Sikeler <daniel.sikeler@muenchen.de>
2017-12-14 10:51:47 +0100

    LocalStorage Änderungen eingebaut

--- test/app/services/local-storage.service.spec.ts
+++ test/app/services/local-storage.service.spec.ts
@@ -1,17 +1,16 @@
 import { inject, TestBed } from '@angular/core/testing';
-
-import { LocalStorageService } from './local-storage.service';
+import { LocalStorageService } from '../../../src/app/services/local-storage.service';
+import { DexieStorage } from '../../../src/app/storage/dexie-storage';
 
 describe('LocalStorageService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [LocalStorageService]
+      providers: [DexieStorage, LocalStorageService]
     });
   });
 
   it('should be created', inject([LocalStorageService], (service: LocalStorageService) => {
     expect(service).toBeTruthy();
   }));
-
 
 });
