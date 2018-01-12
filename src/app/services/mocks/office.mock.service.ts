d07d8d6056fef2f0f2112e7d0a0af339b7bc4815
Eymux <eymux2009@gmail.com>
2018-01-12 09:38:05 +0100

    Und hier ist die eigentliche Mock-Klasse.

---
+++ src/app/services/mocks/office.mock.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Logger } from '@nsalaun/ng-logger';
+
+@Injectable()
+export class OfficeMockService {
+
+  constructor(private log: Logger) { }
+
+  async openDocument(base64: string): Promise<void> {
+    this.log.debug('OfficeMockService.openDocument');
+  }
+
+  async getFragmentNames(): Promise<string[]> {
+    this.log.debug('OfficeMockService.getFragmentNames');
+    return Promise.resolve([
+      'Adresse_Angaben',
+      'Email',
+      'Empfaengerfeld',
+      'Fensterzeile',
+      'Fusszeile_Dateinname',
+      'Fusszeile_Rahmen',
+      'Hauptabteilung',
+      'Logo',
+      'Zusatz'
+    ]);
+  }
+
+  async insertFragment(name: string, base64: string): Promise<void> {
+    this.log.debug('OfficeMockService.insertFragment');
+  }
+
+}
