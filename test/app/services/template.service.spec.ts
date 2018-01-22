5cc245df7955817f56be6e9c2848075fe04869b4
Eymux <eymux2009@gmail.com>
2018-01-22 11:36:47 +0100

    Fix für fehlende Provider in Test.

--- test/app/services/template.service.spec.ts
+++ test/app/services/template.service.spec.ts
@@ -1,11 +1,21 @@
-import { TestBed, inject } from '@angular/core/testing';
-
-import { TemplateService } from './template.service';
+import { inject, TestBed } from '@angular/core/testing';
+import { TemplateService } from '../../../src/app/services/template.service';
+import { HttpModule } from '@angular/http';
+import { environment } from '../../../src/environments/environment';
+import { OfficeService } from '../../../src/app/services/office.service';
+import { NgLoggerModule } from '@nsalaun/ng-logger';
 
 describe('TemplateService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [TemplateService]
+      imports: [
+        HttpModule,
+        NgLoggerModule.forRoot(environment.loglevel)
+      ],
+      providers: [
+        TemplateService,
+        { provide: OfficeService, useClass: environment.officeServie }
+      ]
     });
   });
 
