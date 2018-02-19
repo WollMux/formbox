8d9d9a1fc69744d4bfa2820100aaccddd89881a9
Daniel Sikeler <daniel.sikeler@muenchen.de>
2018-02-19 14:01:37 +0100

    Absender Verwalten Komponente

---
+++ src/app/components/absender-verwalten/absender-verwalten.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { select } from '@angular-redux/store';
+import { Observable } from 'rxjs/Observable';
+import { Logger } from '@nsalaun/ng-logger';
+
+import { Absender } from '../../storage/absender';
+import { AbsenderlisteActions } from '../../store/actions/absenderliste-actions';
+import { StorageActions } from '../../store/actions/storage-actions';
+
+@Component({
+  selector: 'app-absender-verwalten',
+  templateUrl: './absender-verwalten.component.html',
+  styleUrls: ['./absender-verwalten.component.css']
+})
+export class AbsenderVerwaltenComponent implements OnInit, OnDestroy {
+  @select([ 'absenderliste', 'pal']) pal: Observable<Absender[]>;
+
+  constructor(
+    private log: Logger,
+    private actions: AbsenderlisteActions,
+    private storageActions: StorageActions
+  ) { }
+
+  ngOnInit(): void {
+    // nothing to initialize
+  }
+
+  ngOnDestroy(): void {
+    this.storageActions.updatePAL();
+  }
+
+  addToPALByID = (actions: AbsenderlisteActions): (id: number) => void => {
+    return (id: number) => {
+      actions.addAbsenderByID(id);
+    };
+  }
+
+  addToPAL(event: any): void {
+    this.actions.addAbsender(event.dragData);
+  }
+
+  removeFromPAL(id: number): void {
+    this.actions.removeAbsender(id);
+  }
+
+  editAbsender(id: number): void {
+    this.log.debug(`edit ${id}`);
+  }
+}
