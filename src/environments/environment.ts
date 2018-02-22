82405944f966e68f0c9942ad1ae8e0d7fb62c2e0
Daniel Sikeler <daniel.sikeler@muenchen.de>
2018-02-22 16:05:41 +0100

    Zyklische Abhängigkeit aufgelöst

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,11 @@
 
 import { Level } from '@nsalaun/ng-logger';
 import { OfficeService } from '../app/services/office.service';
-import { LDAPService } from '../app/services/ldap.service';
 
 export const environment = {
   test: false,
   production: false,
   formboxapi: 'https://localhost:4201',
   loglevel: Level.LOG,
-  officeService: OfficeService,
-  ldapService: LDAPService
+  officeService: OfficeService
 };
