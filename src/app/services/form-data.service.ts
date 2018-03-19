32b5b969e9e245fdf9a4b28b5eb231bb522e8a8d
Eymux <eymux2009@gmail.com>
2018-03-19 11:40:21 +0100

    Änderungen aus Review.

--- src/app/services/form-data.service.ts
+++ src/app/services/form-data.service.ts
@@ -10,6 +10,7 @@ import { FormXmlParserService } from './form-xml-parser.service';
 export class FormDataService {
   static readonly namespace = 'http://www.muenchen.de/formbox/forms';
 
+  // TODO: Für Testzwecke. Kann gelöscht werden, wenn die Testdaten nicht mehr gebraucht werden.
   xml = `<form xmlns="http://www.muenchen.de/formbox/forms"
       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
       xsi:schemaLocation="http://www.muenchen.de/formbox/forms http://www.muenchen.de/formbox/form.xsd">
