7386cf2ddd21968edf44fcab0f729fb906795bf0
Daniel Sikeler <daniel.sikeler@muenchen.de>
2018-05-08 09:52:48 +0200

    Alles nach String konvertieren bevor es in Office eingefügt wird

--- src/app/services/office.service.ts
+++ src/app/services/office.service.ts
@@ -150,7 +150,7 @@ export class OfficeService {
 
       return context.sync().then(() => {
         if (value) {
-          control.insertText(value, 'Replace');
+          control.insertText(String(value), 'Replace');
         }
         control.delete(true);
 
