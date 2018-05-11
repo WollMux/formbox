dc377be42ddb792836ed21c175cba9dc3a99a94a
Eymux <eymux2009@gmail.com>
2018-05-11 14:20:39 +0200

    Funktionen zum Ein- Und Ausschalten von Verfuegungspunkten.

--- src/app/services/sachleitende-verfuegung.service.ts
+++ src/app/services/sachleitende-verfuegung.service.ts
@@ -5,6 +5,8 @@ import { OfficeService } from './office.service';
 
 @Injectable()
 export class SachleitendeVerfuegungService {
+  private static readonly FORMATVORLAGE = 'FormboxVerfuegungspunkt';
+
   private document = undefined;
 
   constructor(private log: Logger, private office: OfficeService) { /* Leer */ }
@@ -43,5 +45,46 @@ export class SachleitendeVerfuegungService {
     }
 
     return Promise.reject('Kein Document offen.');
+  }
+
+  async toggleVerfuegungspunkt(): Promise<{ id: number, delete: boolean }> {
+    this.log.debug('SachleitendeVerfuegungService.toggleVerfuegungspunkt()');
+
+    return this.office.isInsideContentControl().then(cc => {
+      if (cc && cc.tag === 'SLV') {
+        return this.removeVerfuegungspunkt(cc.id).then(id => ({ id: cc.id, delete: true }));
+      } else {
+        return this.insertVerfuegungspunkt().then(id => ({ id: id, delete: false }));
+      }
+    });
+  }
+
+  async insertVerfuegungspunkt(): Promise<number> {
+    return this.office.expandRangeToParagraph().then(range => {
+      return this.office.insertContentControl('', 'SLV', SachleitendeVerfuegungService.FORMATVORLAGE, range).then(id => {
+        this.office.untrack(range);
+
+        return id;
+      });
+    });
+  }
+
+  async removeVerfuegungspunkt(id: number): Promise<void> {
+    return this.office.deleteContentControl(id);
+  }
+
+  async getVerfuegungspunktText(id: number): Promise<string> {
+    return this.office.getContentControlText(id);
+  }
+
+  async updateVerfuegungspunkt(id: number, text: string, ordinal?: string): Promise<void> {
+    let s;
+    if (ordinal) {
+      s = `${ordinal}\t${text}`;
+    } else {
+      s = text;
+    }
+
+    return this.office.replaceTextInContentControl(id, s);
   }
 }
