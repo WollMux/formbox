9dbd1423e29483236f74d1932c0e83295e9f48ec
bjoernranft <externer.dl.ranft@muenchen.de>
2018-06-08 16:49:11 +0200

    Bindungen Formular-GUI.

--- src/app/components/formular-gui/formular-gui.component.ts
+++ src/app/components/formular-gui/formular-gui.component.ts
@@ -1,26 +1,23 @@
 import { select } from '@angular-redux/store';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 import { Form } from '../../data/forms/form';
 import { FormXmlParserService } from '../../services/form-xml-parser.service';
 import { TemplateActions } from '../../store/actions/template-actions';
+import { FormularGuiActions } from '../../store/actions/formular-gui-actions';
 
 @Component({
   selector: 'app-formular-gui',
   templateUrl: './formular-gui.component.html',
   styleUrls: ['./formular-gui.component.css']
 })
-export class FormularGuiComponent {
+export class FormularGuiComponent implements OnInit {
   @select(['formularEditor', 'form']) form: Observable<Form>;
 
-  onModelChange(val, id): void {
-    // id = id ContentControl
-    // this.templateActions.insertValue(val, id);
+  ngOnInit(): void {
+    this.formularGuiActions.initBindings();
   }
 
-  constructor(
-    private templateActions: TemplateActions,
-    private xmlParser: FormXmlParserService) {
-  }
+  constructor(private formularGuiActions: FormularGuiActions) { }
 }
