0c4671918f22af0203a8e1552500ce3921496365
bjoernranft <externer.dl.ranft@muenchen.de>
2018-06-08 16:49:11 +0200

    Bindungen Formular-GUI.

--- src/app/components/formular-gui-textfield/formular-gui-textfield.component.ts
+++ src/app/components/formular-gui-textfield/formular-gui-textfield.component.ts
@@ -3,6 +3,7 @@ import { Logger } from '@nsalaun/ng-logger';
 
 import { Textfield } from '../../data/forms/textfield';
 import { FormularGuiBase } from '../formular-gui-base/formular-gui-base';
+import { FormularGuiActions } from '../../store/actions/formular-gui-actions';
 
 @Component({
   selector: 'app-formular-gui-textfield',
@@ -11,8 +12,12 @@ import { FormularGuiBase } from '../formular-gui-base/formular-gui-base';
 })
 export class FormularGuiTextfieldComponent extends FormularGuiBase<Textfield> {
 
-  constructor(log: Logger) {
+  constructor(log: Logger, private formGuiActions: FormularGuiActions) {
     super(log);
+  }
+
+  onModelChange(text, ccid): void {
+    this.formGuiActions.updateCCText(text, ccid);
   }
 
 }
---
+++ src/app/services/formular-gui.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { OfficeService } from './office.service';
+import { FormControl } from '../data/forms/form-control';
+import { Form } from '../data/forms/form';
+import { Observable } from 'rxjs/Observable';
+import { Logger } from '@nsalaun/ng-logger';
+import { Control } from '../data/forms/control';
+
+/**
+ * Service für die Formular-GUI.
+ */
+@Injectable()
+export class FormularGuiService {
+
+    constructor(
+        private office: OfficeService,
+        private log: Logger
+    ) { }
+
+    async initBindings(form: Form): Promise<void> {
+        return form.controls.forEach(cc => {
+            if (cc instanceof FormControl) {
+                const formControl = cc as FormControl;
+                this.office.getBindingById(formControl.ccid.toString()).then((res: Office.AsyncResult) => {
+                    if (res.status === Office.AsyncResultStatus.Failed) {
+                        return this.office.addBindingFromNamedItem(formControl.id, formControl.ccid.toString()).then(bindingId => {
+                            return this.office.addEventHandlerToBinding(bindingId, text => {
+                                // Binding CC -> FormGui
+                            }).then(result => {
+                                return result;
+                            });
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    async updateValues(form: Form): Promise<Form> {
+        form.controls.forEach(control => {
+            if (control instanceof FormControl) {
+                return this.office.getContentControlText(control.ccid).then(cc => {
+                    control.value = cc;
+                });
+            }
+        });
+
+        return form;
+    }
+
+    async updateCCText(text: string, ccid: number): Promise<void> {
+        return this.office.setData(text, ccid.toString()).then(res => {
+            return res;
+        });
+    }
+}
