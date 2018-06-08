6493515da9340368facfbd4d7401c3e86cf790db
bjoernranft <externer.dl.ranft@muenchen.de>
2018-06-08 17:13:51 +0200

    Bindung Formular-GUI Textarea-Control.

--- src/app/components/formular-gui-textarea/formular-gui-textarea.component.ts
+++ src/app/components/formular-gui-textarea/formular-gui-textarea.component.ts
@@ -3,6 +3,7 @@ import { Logger } from '@nsalaun/ng-logger';
 
 import { Textarea } from '../../data/forms/textarea';
 import { FormularGuiBase } from '../formular-gui-base/formular-gui-base';
+import { FormularGuiActions } from '../../store/actions/formular-gui-actions';
 
 @Component({
   selector: 'app-formular-gui-textarea',
@@ -11,8 +12,12 @@ import { FormularGuiBase } from '../formular-gui-base/formular-gui-base';
 })
 export class FormularGuiTextareaComponent extends FormularGuiBase<Textarea> {
 
-  constructor(log: Logger) {
+  constructor(log: Logger, private formGuiActions: FormularGuiActions) {
     super(log);
+  }
+
+  onModelChange(text, ccid): void {
+    this.formGuiActions.updateCCText(text, ccid);
   }
 
 }
