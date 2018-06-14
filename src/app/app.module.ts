1d8426fdf3adfd0fec246f7268fab0137a2ba51f
bjoernranft <externer.dl.ranft@muenchen.de>
2018-06-14 16:43:52 +0200

    Redux-Remote-Debugging.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -71,6 +71,7 @@ import { FormularGuiActions } from './store/actions/formular-gui-actions';
 import { KomfortdruckComponent } from './components/komfortdruck/komfortdruck.component';
 import { OnCreateDirective } from './directives/on-create.directive';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { composeWithDevTools } from 'remote-redux-devtools';
 
 @NgModule({
   declarations: [
@@ -157,7 +158,8 @@ export class AppModule {
     if (environment.production || !environment.test) {
       ngRedux.configureStore(rootReducer, INITIAL_STATE, middleware);
     } else {
-      ngRedux.configureStore(rootReducer, INITIAL_STATE, middleware, devTools.enhancer());
+      const composeEnhancers = composeWithDevTools({ realtime: true });
+      ngRedux.provideStore(createStore(rootReducer, INITIAL_STATE, composeEnhancers(applyMiddleware(...middleware))));
     }
 
     this.init.initSLV();
