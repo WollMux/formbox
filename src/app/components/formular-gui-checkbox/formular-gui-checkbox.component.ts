0246d34cae5af072b682be2fe307434dcfc1b41e
bjoernranft <externer.dl.ranft@muenchen.de>
2018-06-20 14:34:09 +0200

    Form-Gui-Checkbox schreibt Wert in Content-Control.

--- src/app/components/formular-gui-checkbox/formular-gui-checkbox.component.ts
+++ src/app/components/formular-gui-checkbox/formular-gui-checkbox.component.ts
@@ -3,6 +3,7 @@ import { Logger } from '@nsalaun/ng-logger';
 
 import { Checkbox } from '../../data/forms/checkbox';
 import { FormularGuiBase } from '../formular-gui-base/formular-gui-base';
+import { FormularGuiActions } from '../../store/actions/formular-gui-actions';
 
 @Component({
   selector: 'app-formular-gui-checkbox',
@@ -11,8 +12,18 @@ import { FormularGuiBase } from '../formular-gui-base/formular-gui-base';
 })
 export class FormularGuiCheckboxComponent extends FormularGuiBase<Checkbox> {
 
-  constructor(private log: Logger) {
+  constructor(private log: Logger, private formGuiActions: FormularGuiActions) {
     super();
+  }
+
+  onModelChange(text, ccid): void {
+    if (text) {
+      text = '\u2611';
+    } else {
+      text = '\u2610';
+    }
+
+    this.formGuiActions.updateContentControlText(text, ccid);
   }
 
 }
