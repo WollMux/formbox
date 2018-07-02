3f170c17f513603e5a0effd209f17f2bcb396d64
Eymux <eymux2009@gmail.com>
2018-07-02 11:04:58 +0200

    Tests für Verfügungspunkt1.

--- test/app/store/middleware/sachleitendeverfuegung-epics.spec.ts
+++ test/app/store/middleware/sachleitendeverfuegung-epics.spec.ts
@@ -16,7 +16,6 @@ import { FormBoxState, INITIAL_STATE } from '../../../../src/app/store/states/fo
 import { OfficeMockService } from '../../../../src/app/services/mocks/office.mock.service';
 import configureStore from 'redux-mock-store'; // tslint:disable-line no-implicit-dependencies
 
-
 describe('Sachleitende Verfuegung epics', () => {
   let mockStore;
 
@@ -93,4 +92,42 @@ describe('Sachleitende Verfuegung epics', () => {
       }
     )));
 
+  it('toggle on Verfuegungspunkt1',
+    async(inject([SachleitendeverfuegungEpics, SachleitendeVerfuegungService],
+      (epics: SachleitendeverfuegungEpics, service: SachleitendeVerfuegungService) => {
+        const store: NgRedux<FormBoxState> = mockStore(INITIAL_STATE);
+        store.getState().slv.slv.verfuegungspunkt1 = 1;
+        spyOn(service, 'isVerfuegungspunkt1').and.returnValue(Promise.resolve(true));
+        const spy = spyOn(service, 'toggleVerfuegungspunkt1').and.returnValue(Promise.resolve({ id: 1, delete: false }));
+
+        const action = SachleitendeverfuegungActions.TOGGLE({ abdruck: false });
+        const p = epics.toggling(ActionsObservable.of(action), store);
+
+        p.scan((arr, value) => [...arr, value], [])
+          .takeLast(1)
+          .subscribe(result => {
+            expect(spy).toHaveBeenCalled();
+            expect(result[0]).toEqual({ type: 'INSERT_VERFUEGUNGSPUNKT_STARTED', payload: { id: 1, delete: false } });
+            expect(result[1]).toEqual({ type: 'RENUMBER_STARTED', payload: {} });
+          });
+      }
+    )));
+
+  it('toggle off Verfuegungspunkt1',
+    async(inject([SachleitendeverfuegungEpics, SachleitendeVerfuegungService],
+      (epics: SachleitendeverfuegungEpics, service: SachleitendeVerfuegungService) => {
+        const store: NgRedux<FormBoxState> = mockStore(INITIAL_STATE);
+        store.getState().slv.slv.verfuegungspunkt1 = 1;
+        spyOn(service, 'isVerfuegungspunkt1').and.returnValue(Promise.resolve(true));
+        const spy = spyOn(service, 'toggleVerfuegungspunkt1').and.returnValue(Promise.resolve({ id: 1, delete: true }));
+
+        const action = SachleitendeverfuegungActions.TOGGLE({ abdruck: false });
+        const p = epics.toggling(ActionsObservable.of(action), store);
+
+        p.subscribe(result => {
+          expect(spy).toHaveBeenCalled();
+          expect(result).toEqual({ type: 'DELETE_VERFUEGUNGSPUNKT_STARTED', payload: 1 });
+        });
+      }
+    )));
 });
