cda2ef33c932e645b8b0ef9808c59f25faae5518
bjoernranft <externer.dl.ranft@muenchen.de>
2018-07-03 13:52:33 +0200

    Änderungen gemäß Review.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
   production: false,
   formboxapi: 'https://localhost:4201',
   loglevel: Level.LOG,
-  reduxRemoteName: 'BJR',
+  // reduxRemoteName: '',
   reduxRemoteUrl: 'kvm-javabuild.tvc.muenchen.de',
   reduxRemotePort: 12556,
   reduxRemoteSecure: true
